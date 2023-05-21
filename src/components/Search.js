5450492b5b9877d7bd667023ec85ea8c37166016
Rushit <rushitparmar59@gmail.com>
2023-05-21 17:14:15 -0500

    github

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,38 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import githubUsernameRegex from 'github-username-regex';
+import axios from 'axios';
 
 function Search() {
   const navigateToUser = useNavigate();
   const [input, setInput] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
 
+  let userEndPoint = `https://api.github.com/users/${input}`;
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (input.length > 1 && githubUsernameRegex.test(input)) {
-      navigateToUser(`/user/${input}`);
-    } else {
-      setErrorMsg('Username not found. Try again.');
+    if (input.length < 2 || !githubUsernameRegex.test(input)) {
+      setErrorMsg('Invalid input. Try again.');
       setInput("");
+    } else {
+      checkData();
     }
+  }
 
+
+  const checkData = async () => {
+    try {
+      const userResponse = await axios.get(userEndPoint);
+      if (userResponse.data) {;
+        navigateToUser(`/user/${input}`);
+        setErrorMsg('');
+        setInput('');
+      }
+    } catch {
+      setErrorMsg('Username not found. Try again.');
+      setInput('');
+    }
   }
 
   const handleChange = (event) => {
